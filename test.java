369ca425d72f46a1de380ac44e28ba45a928d46d
Mauricio Eduardo Zegarra Puma <mzegarrapu@unsa.edu.pe>
2023-06-17 11:37:49 -0500

    Agregado codigo para probar Front y Back en test

--- test.java
+++ test.java
@@ -5,17 +5,21 @@ public class test {
         cola.enqueue(4,5);
         cola.enqueue(1,7);
         cola.enqueue(7,6);
-        cola.enqueue(5,8);
+        cola.enqueue(5,37);
         cola.enqueue(11,8);
-        cola.enqueue(6,8);
-        cola.enqueue(-4,8);
-        cola.enqueue(0,8);
-        cola.enqueue(-1,8);
-        cola.enqueue(6,8);
-        cola.enqueue(3,8);
+        cola.enqueue(6,100);
+        cola.enqueue(-4,96);
+        cola.enqueue(0,3);
+        cola.enqueue(-1,41);
+        cola.enqueue(6,59);
+        cola.enqueue(3,11);
         cola.enqueue(8,1);
 
         System.out.println(cola);
+
+        System.out.println("La mayor prioridad es de: " + cola.Front().getKey() + " y pertenece al elemento con la informacion: " + cola.Front().getData());
+
+        System.out.println("La menor prioridad es de: " + cola.Back().getKey() + " y pertenece al elemento con la informacion: " + cola.Back().getData());
 
     }
 }
